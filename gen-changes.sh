86c4cab58b70dd3a3c569e753afea60fd5e550a0
Elan Ruusamäe <glen@pld-linux.org>
2012-02-29 05:21:56 +0000

    - script to make changelogs
    Changed files:
        gen-changes.sh -> 1.1

---
+++ gen-changes.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+p=closure-compiler
+svn=http://$p.googlecode.com/svn/trunk
+
+set -x
+set -e
+x=$-
+
+# create svn diff and svn log based on revision ranges for each release
+svnlogs() {
+	set -$x
+	local date=$1 range=$2
+	test -e changes.$date.txt || {
+		svn diff $svn -r$range > svn.tmp && mv svn.tmp changes.$date.txt
+	}
+	test -e log.$date.txt || {
+		svn log $svn -r$range > svn.tmp && mv svn.tmp log.$date.txt
+	}
+}
+
+svnlogs 20111114 1459:1592
+svnlogs 20111003 1346:1459
+svnlogs 20110811 1314:1346
+svnlogs 20110804 1180:1314
